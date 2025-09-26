89302c21f9badc753c19c3872a464811e3cb7714
Pedro Santos Kajiya <kajiyapedro@gmail.com>
2025-09-26 08:10:39 -0300

    Feat: liberação de CORS

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import asyncio
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.agent import create_runner
 from app.api.routes import router as api_router
 from app.rag.rag_service import ingest_documents, build_vectorstore, get_vectorstore, CHROMA_PATH
 
 app = FastAPI(title="nutriXpert")
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
